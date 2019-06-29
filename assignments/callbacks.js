406b8e4cc159226a947b84e4bc1fe336eae7ed57
Your Name <you@example.com>
2019-06-29 13:33:47 -0500

    worked through my array-methods and callbacks. working on closure, and will go back through to do the stretch goals.

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,8 +1,10 @@
-// Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
+// Create a higher order function and invoke the callback function to test your work. 
+// You have been provided an example of a problem and a solution to see how this works with our items array.  
+// Study both the problem and the solution to figure out the rest of the problems.
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
-/* 
+
 
   //Given this problem: 
   
@@ -22,23 +24,43 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
     console.log(first)
   });
 
-*/
+
 
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+  getLength(items, function(length) {
+    console.log(length)
+  });
 
 function last(arr, cb) {
+  return cb(arr[arr.length - 1]);
   // last passes the last item of the array into the callback.
 }
+last(items, function(last) {
+  console.log(last)
+});
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  add(x,y) 
+  return cb(x,y);
 }
+function add(x, y) {
+  return x + y;
+}
+console.log(add(1,2));
+
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  multiply(x,y)
+  return cb(x,y);
+}
+function multiply(x,y) {
+  return x + y;
 }
 
 function contains(item, list, cb) {
--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,18 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+function popMusicFemales () {
+  let superStar = "Madonna";
+  console.log(`${superStar} is the queen that paved the way`);
 
+    function popCulture() {
+      let mediumStar = "Lady Gaga";
+      console.log(`${mediumStar} has created a name for herself in ${superStar}'s footprints`);
+
+        function popMusic () {
+          let star = "Ariana Grande";
+        }
+    }
+}
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
